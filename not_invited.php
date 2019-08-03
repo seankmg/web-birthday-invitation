2bf6d591682b523037d44411fdcb85508b55926e
seankmg <32743782+seankmg@users.noreply.github.com>
2019-08-03 21:38:17 +0200

    Create not_invited.php
    File that has the html text of the login rejection in german language

---
+++ not_invited.php
@@ -0,0 +1,39 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=0.9, maximum-scale=0.9, user-scalable=no" />
+<meta name="viewport" content="initial-scale=0.9, maximum-scale=0.9" media="(device-height: 568px)" />
+<link rel="stylesheet" type="text/css" href="/error_docs/style.css">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link href="https://fonts.googleapis.com/css?family=Mr+Dafoe&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700&display=swap" rel="stylesheet">
+</head>
+<style>
+body{
+background-color: black;
+}
+.mrdafoe{
+font-family: 'Mr Dafoe', cursive;
+color: white;
+}
+.montserrat{
+font-family: 'Montserrat', sans-serif;
+color: white;
+}
+.btnCheck {
+width: 200px;
+height: 50px;
+background-color: purple;
+border-width: 2px;
+border-color: #36F5E5; 
+color: white;
+font-size: 11pt;
+font-weight: 400;
+}
+</style>
+<body>
+<center>
+<br><br>
+<p class="mrdafoe" style="font-size: 50pt;">Du bist nicht<br>eingeladen!</p>
+<p class="montserrat">Sicher, dass deine Angaben korrekt waren?</p>
+<br><br><br>
+<button class="btnCheck" type="submit" onclick="window.location.href='https://invite.seankmg.de'">Erneut versuchen</button>
+</body>
